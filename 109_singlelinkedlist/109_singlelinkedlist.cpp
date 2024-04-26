499fb89a281da0a6fafd5c5c178f122f1ca25662
ichool <faisalaja2653@gmail.com>
2024-04-26 11:05:01 +0700

    membuat switch

--- 109_singlelinkedlist/109_singlelinkedlist.cpp
+++ 109_singlelinkedlist/109_singlelinkedlist.cpp
@@ -144,3 +144,20 @@ int main() {
             cout << "5. Keluar" << endl;
             cout << "Pilihan: ";
             cin >> pilihan;
+            switch (pilihan)
+            {
+            case 1:
+                addNode();
+                cout << "Data Berhasil Ditambahkan" << endl;
+                system("pause");
+                system("cls");
+                break;
+            case 2:
+                if (listEmpty())
+                    cout << "List kosong" << endl;
+                system("pause");
+                system("cls")
+                break;
+            }
+
+
